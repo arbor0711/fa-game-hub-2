2288ddc41b4181604ed0bba3774f1d9418125ebf
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-31 16:25:08 +0200

    Fetching games with ReactQuery

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,8 @@ function GameGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  if (error) return <Text>{error}</Text>;
+  //the error "Type '{}' is not assignable to type 'ReactNode'." means that react does not know how to render this error message. So to fix this I have to go back to my hook and provide generic type argument
+  if (error) return <Text>{error.message}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -27,7 +28,7 @@ function GameGrid({ gameQuery }: Props) {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,10 @@
-import useData from "./useData";
+import { FetchResponse } from "./useData";
 import { GameQuery } from "./../App";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+
+//TODO:I have the same interface in usePlatforms. But the point is that now I do not distracted from my own job. I should have a notebook on my side and when I see a bug or sth to fix, I just write it to do not forget it later, and then I'll come back and resolve it.
+// FIXME: Fix this duplication
 export interface Platform {
   id: number;
   name: string;
@@ -14,17 +19,19 @@ export interface Game {
   genres: string;
 }
 const useGames = (gameQuery: GameQuery) =>
-  // In here I pass selectedGenre as query string parameter to the data hook
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
+
 export default useGames;
