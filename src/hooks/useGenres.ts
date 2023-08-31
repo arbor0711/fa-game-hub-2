27519b602f44170886a0886c2fd4f4f78512f44e
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-08-31 16:50:35 +0200

    Refactoring fetch games with React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -15,10 +15,7 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: () =>
       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
-    // With below line I say the browser to keep the data 24h in its cache and then refresh it
     staleTime: 24 * 60 * 60 * 1000, //24h
-    // I can provide initial data, So I don't have to go to the backend and show to user a spinner. I can set the initial data to genres that I provided before
-    // Now I have compilation error ===> I solve this like below
     initialData: { count: genres.length, results: genres },
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -10,7 +10,6 @@ export interface Platform {
 const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
-    // It's very important that remember that in queryFn you should add a function, not getting the result. So I should add a function that finally could retrieve the data.
     queryFn: () =>
       apiClient
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
