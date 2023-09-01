0225c15811bdf006ce0413fb2d07e94b3f01644b
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-09-01 14:36:17 +0200

    Implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { GameQuery } from "../App";
-import React from "react";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
   gameQuery: GameQuery;
@@ -25,14 +26,30 @@ function GameGrid({ gameQuery }: Props) {
   //the error "Type '{}' is not assignable to type 'ReactNode'." means that react does not know how to render this error message. So to fix this I have to go back to my hook and provide generic type argument
   if (error) return <Text>{error.message}</Text>;
   return (
-    <Box padding={"15px"}>
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+    <InfiniteScroll
+      // For dataLength Mosh used another method. He, firstly, set a const named "fetchedGamesCount" and define it outside return, after error handling.
+      // const fetchedGamesCount= data?.pages.reduce((total, page) => total + page.results.length, 0) || 0
+      // But I think that I could count the pages inside dataLength, like below ===>
+      dataLength={data?.pages.length || 0}
+      hasMore={!!hasNextPage}
+      next={fetchNextPage}
+      // Very nice to know
+      // If you have undefined value and you put !! in front of that you will got false. So when you have a value that is undefined or true, and you only want boolean, then you could use !! to transform it to boolean value.
+      loader={<Spinner color="blue" my={7} />}
+      endMessage={<Text>No more data to load.</Text>}
+    >
+      <SimpleGrid
+        padding={"15px"}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
+
         {data?.pages.map((page, index) => (
           <React.Fragment key={index}>
             {page.results.map((game) => (
@@ -43,12 +60,7 @@ function GameGrid({ gameQuery }: Props) {
           </React.Fragment>
         ))}
       </SimpleGrid>
-      {hasNextPage && (
-        <Button my={5} onClick={() => fetchNextPage()}>
-          {isFetchingNextPage ? "Loading..." : "Load More"}
-        </Button>
-      )}
-    </Box>
+    </InfiniteScroll>
   );
 }
 
