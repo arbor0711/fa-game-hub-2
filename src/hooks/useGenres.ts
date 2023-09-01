5bfccbe8a10e6ada30b62716e8652f6cdb9dca29
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-09-01 09:15:50 +0200

    Refactoring- create a reusable API client

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,9 +1,9 @@
 // import useData from "./useData";
-import genres from "../data/genre";
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 import genre from "../data/genre";
+import APIClient from "../services/api-client";
+
+const apiClient = new APIClient<Genre>("/genres");
 export interface Genre {
   id: number;
   name: string;
@@ -13,10 +13,8 @@ export interface Genre {
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
-    staleTime: 24 * 60 * 60 * 1000, //24h
-    initialData: { count: genres.length, results: genres },
+    queryFn: apiClient.getAll,
+    initialData: { count: genre.length, results: genre },
   });
 
 export default useGenres;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,14 +1,30 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   // Whit this configuration this key will be included in the query string of every http request I'll send to my backend
   params: {
     key: "55d88e6322e74f74a4ef6bfa5b660721",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
