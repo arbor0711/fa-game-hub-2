a821e37dbe0f9e2e0d2d33cebfa59866ea174b29
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-09-07 13:25:45 +0200

    Define a custom hook with Zustand

---
+++ src/store.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+
+interface GameQuery {
+  genreId?: number;
+  platformId?: number;
+  sortOrder?: string;
+  searchText?: string;
+}
+
+// Why I decided to do not have a single function for setting gameQuery? The reason is that the logic for updating the search text is different from other properties.
+interface GameQueryStore {
+  gameQuery: GameQuery;
+  setSearchText: (searchText: string) => void;
+  setGenreId: (genreId: number) => void;
+  setPlatformId: (platformId: number) => void;
+  setSortOrder: (sortOrder: string) => void;
+}
+const useGameQueryStore = create<GameQueryStore>()((set) => ({
+  gameQuery: {},
+  //   When the user search sth, I reset all filters to be able to show the correct results
+  setSearchText: (searchText) => set(() => ({ gameQuery: { searchText } })),
+  setGenreId: (genreId) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
+  setPlatformId: (platformId) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
+  setSortOrder: (sortOrder) =>
+    set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
+}));
+
+export default useGameQueryStore;
