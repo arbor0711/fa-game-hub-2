78c7db92d3e3bb6efcf86ab80bc80c007f8be1f1
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-09-18 16:50:35 +0200

    Fix resetting all queries by clicking page icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,12 @@ import logo from "../assets/pacman-25805.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
+import useGameQueryStore from "../store";
 
 function NavBar() {
   return (
     <HStack padding={"10px"}>
-      <Link to="/">
+      <Link to="/" onClick={() => useGameQueryStore()}>
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
       <SearchInput />
