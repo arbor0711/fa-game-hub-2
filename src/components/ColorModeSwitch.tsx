ccdb4ba0266dd77f79c6f310f205cfe1e37bf6ab
Alireza <42074597+arbor0711@users.noreply.github.com>
2023-09-18 21:31:15 +0200

    commit

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { HStack, useColorMode } from "@chakra-ui/react";
-=======
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
-import { useState } from "react";
-import { SunIcon, MoonIcon } from "@chakra-ui/icons";
->>>>>>> e427576c068583edc1d9c615b3098b874642ceed
 
 function ColorModeSwitch() {
   const { toggleColorMode, colorMode } = useColorMode();
